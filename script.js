9a9edb110f67d04c3033e7d2398a3636abc0b132
TimothyBowen <autisticgamelover@gmail.com>
2020-12-10 20:20:19 +0000

    hack

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// KFVM-GQLG-FCFE-QIBY-QEIT
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function hacker() {
-  var hackObj = {data: "KFVM-GQLG-FCFE-QIBY-QEIT", status: "encoded"};
+  var hackObj = {data: "VmliZSBvdmVyIHRvICIvaGFja21lLmh0bWwi", status: "encoded"};
   return hackObj;
 }
 
