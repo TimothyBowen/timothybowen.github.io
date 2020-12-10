e820a4c740cca5eb565db013c53355765f33665a
Timothy Bowen <43269979+TimothyBowen@users.noreply.github.com>
2020-12-10 13:32:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,5 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function main() {
-  await sleep(10000);
-  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-}
+async function main() {}
 main();
